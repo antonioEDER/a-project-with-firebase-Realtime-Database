d1b2539aaad3dd6bfbd562f7ac3d259a3ca68d7f
Eder Antonio <ederantonio@eucatur.com.br>
2022-03-17 20:24:42 -0400

    Adicionando imagens

--- app/js/todo.js
+++ app/js/todo.js
@@ -3,11 +3,13 @@ import firebase from "firebase";
 var ulTodoList = document.getElementById('ulTodoList')
 var todoForm = document.getElementById('todoForm')
 var todoCount = document.getElementById('todoCount')
+var progress = document.getElementById('progress')
+var progressFeedback = document.getElementById('progressFeedback')
+progressFeedback.style.display = "none";
 
 todoForm.onsubmit = function (event) {
   event.preventDefault(); // Evita o redirecionamento da página
   if (todoForm.name.value != "") {
-
     var file = todoForm.file.files[0] // Seleciona o primeiro aquivo da seleção de aquivos
     if (file != null) { // Verifica se o arquivo foi selecionado
       if (file.type.includes('image')) { // Verifica se o arquivo é uma imagem
@@ -20,12 +22,37 @@ todoForm.onsubmit = function (event) {
         var storageRef = firebase.storage().ref(imgPath)
         
         // Inicia o processo de upload
-        storageRef.put(file)
+        var upload = storageRef.put(file)
+
+        trackUpload(upload).then(function () {
+          storageRef.getDownloadURL().then(function (downloadURL) {
+            console.log('downloadURL', downloadURL);
+            var data = {
+              imgUrl: downloadURL,
+              name: todoForm.name.value,
+              nameLowerCase: todoForm.name.value.toLowerCase()
+            }
+  
+            window.dbRefUsers().ref("users").child(firebase.auth().currentUser.uid).push(data).then(function () {
+              console.log('Tarefa "' + data.name + '" adicionada com sucesso')
+            }).catch(function (error) {
+              showError('Falha ao adicionar tarefa (use no máximo 30 caracteres): ', error)
+            })
+  
+            todoForm.name.value = ''
+            todoForm.file.value = ''
+          })      
+        }).catch(function (error) {
+          showError('Falha ao adicionar tarefa: ', error)
+        })
+      } else {
+        alert('O arquivo selecionado precisa ser uma imagem. Tente novamente')
       }
     }
     
     var data = {
       name: todoForm.name.value,
+      ameLowerCase: todoForm.name.value.toLowerCase()
     };
 
     var postListRef = window.dbRefUsers().ref("users").child(firebase.auth().currentUser.uid)
@@ -50,6 +77,15 @@ function fillTodoList(dataSnapshot) {
   dataSnapshot.forEach(function (item) { // Percorre todos os elementos
     var value = item.val()
     var li = document.createElement('li') // Cria um elemento do tipo li
+
+    var imgLi = document.createElement('img') // Cria um elemento img
+    imgLi.style.height = '50px'
+    imgLi.style.width = '50px'
+    // Configura src (origem da imagem) como sendo o imgUrl da tarefa
+    imgLi.src = value.imgUrl ? value.imgUrl : 'https://toppng.com/uploads/preview/icone-homem-man-icon-11553494437bgj8eja2bq.png'
+    imgLi.setAttribute('class', 'imgTodo') // Defini classes de estilização
+    li.appendChild(imgLi) // Adiciona o img dentro do li
+
     var spanLi = document.createElement('span') // Cria um elemento do tipo span
     spanLi.appendChild(document.createTextNode(value.name)) // Adiciona o elemento de texto dentro da nossa span
     spanLi.id = item.key // Define o id do spanLi como a chave da tarefa
@@ -96,7 +132,8 @@ window.updateTodo = (key) => {
   var newTodoName = prompt('Escolha um novo nome para a tarefa \"' + selectedItem.innerHTML + '\".', selectedItem.innerHTML)
   if (newTodoName != '') {
     var data = {
-      name: newTodoName
+      name: newTodoName,
+      nameLowerCase: newTodoName.toLowerCase()
     }
 
     window.dbRefUsers().ref("users").child(firebase.auth().currentUser.uid).child(key).update(data).then(function () {
@@ -108,5 +145,42 @@ window.updateTodo = (key) => {
     alert('O nome da tarefa não pode ser em branco para atualizar a tarefa')
   }
 }
+
+function trackUpload(upload) {
+  progressFeedback.style.display = "block";
+  upload.on('state_changed',
+    function (snapshot) { // Segundo argumento: Recebe informações sobre o upload
+      console.log(snapshot.bytesTransferred / snapshot.totalBytes * 100 + '%')
+      progressFeedback.innerHTML = `${snapshot.bytesTransferred / snapshot.totalBytes * 100}%`
+    }, function (error) { // Terceiro argumento: Função executada em caso de erro no upload
+      alert(error, 'Falha no upload da imagem')
+    },
+    function () { // Quarto argumento: Função executada em caso de sucesso no upload
+      console.log('Sucesso no upload')
+      // progressFeedback.style.display = "none";
+    })
+}
+
+
+
+// Rastreia o progresso de upload
+function trackUpload(upload) {
+  return new Promise(function (resolve, reject) {
+    progressFeedback.style.display = "block";
+    upload.on('state_changed',
+      function (snapshot) { // Segundo argumento: Recebe informações sobre o upload
+        console.log((snapshot.bytesTransferred / snapshot.totalBytes * 100).toFixed(2) + '%')
+        progressFeedback.innerHTML = `${snapshot.bytesTransferred / snapshot.totalBytes * 100}%`
+      }, function (error) { // Terceiro argumento: Função executada em caso de erro no upload
+        reject(error)
+      },
+      function () { // Quarto argumento: Função executada em caso de sucesso no upload
+        console.log('Sucesso no upload')
+        progressFeedback.style.display = "block";
+        resolve()
+      })
+  })
+}
+
 
 window.OnDataBase = OnDataBase
